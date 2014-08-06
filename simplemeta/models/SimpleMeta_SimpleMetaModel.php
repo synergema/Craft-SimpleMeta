3231a6df2e1a5fe249329834256f0ae31841efa7
Chad Clark <chad@McNulty.local>
2014-08-06 17:17:42 -0400

    Added OG Music - Song

--- simplemeta/models/SimpleMeta_SimpleMetaModel.php
+++ simplemeta/models/SimpleMeta_SimpleMetaModel.php
@@ -8,9 +8,15 @@ class SimpleMeta_SimpleMetaModel extends BaseModel
 		// Open Graph Types
 		$openGraphTypes = array(
 			'article',
-			'audio',
+			'music.song',
+			'music.album',
+			'music.playlist',
+			'music.radio_station',
 			'profile',
 			'video.movie',
+			'video.episode',
+			'video.tv_show',
+			'video.other',
 			'website',
 		);
 
@@ -37,30 +43,41 @@ class SimpleMeta_SimpleMetaModel extends BaseModel
 		);
 
 		return array(
-				'elementId'                          => array(AttributeType::Number, 'default' => null),
-				'handle'                             => array(AttributeType::String, 'default' => null),
-				'socialOGTitle'                      => array(AttributeType::String, 'default' => null),
-				'socialOGDescription'                => array(AttributeType::String, 'default' => null),
-				'socialOGImageId'                    => array(AttributeType::Number, 'default' => null), /* Asset */
-				'socialOGType'                       => array(AttributeType::Enum, 'values' => $openGraphTypes, 'default' => $openGraphTypes[0]),
-					// Open Graph Type fields
-					'socialOGAudioContentId'             => array(AttributeType::Number, 'default' => null), /* Asset */
-					'socialOGAudioContentSecure'         => array(AttributeType::String, 'default' => null),
-					'socialOGAudioType'                  => array(AttributeType::String, 'default' => null),
-					'socialOGProfileFirstName'           => array(AttributeType::String, 'default' => null),
-					'socialOGProfileLastName'            => array(AttributeType::String, 'default' => null),
-					'socialOGProfileUsername'            => array(AttributeType::String, 'default' => null),
-					'socialOGProfileGender'              => array(AttributeType::String, 'default' => null),
-					'socialOGVideoContentId'             => array(AttributeType::Number, 'default' => null), /* Asset */
-					'socialOGVideoContentSecure'         => array(AttributeType::String, 'default' => null),
-					'socialOGVideoUrl'					 => array(AttributeType::String, 'default' => null),
-					'socialOGVideoType'                  => array(AttributeType::String, 'default' => null),
-					'socialOGVideoWidth'                 => array(AttributeType::String, 'default' => null),
-					'socialOGVideoHeight'                => array(AttributeType::String, 'default' => null),
-				'socialTwitterTitle'                 => array(AttributeType::String, 'default' => null),
-				'socialTwitterType'                  => array(AttributeType::Enum, 'values' => $twitterCardTypes, 'default' => $twitterCardTypes[0]),
-				'socialTwitterDescription'           => array(AttributeType::String, 'default' => null),
-					// Twiter Card Type fields
+				'elementId' => array(AttributeType::Number, 'default' => null),
+				'handle'    => array(AttributeType::String, 'default' => null),
+				// Open Graph
+					'socialOGTitle'              => array(AttributeType::String, 'default' => null),
+					'socialOGDescription'        => array(AttributeType::String, 'default' => null),
+					'socialOGImageId'            => array(AttributeType::Number, 'default' => null), /* Asset */
+					'socialOGType'               => array(AttributeType::Enum, 'values' => $openGraphTypes, 'default' => $openGraphTypes[0]),
+					// OG Video
+					'socialOGVideoContentId'     => array(AttributeType::Number, 'default' => null), /* Asset */
+					'socialOGVideoContentSecure' => array(AttributeType::String, 'default' => null),
+					'socialOGVideoUrl'           => array(AttributeType::String, 'default' => null),
+					'socialOGVideoType'          => array(AttributeType::String, 'default' => null),
+					'socialOGVideoWidth'         => array(AttributeType::String, 'default' => null),
+					'socialOGVideoHeight'        => array(AttributeType::String, 'default' => null),
+					// OG Audio
+					'socialOGAudioContentId'     => array(AttributeType::Number, 'default' => null), /* Asset */
+					'socialOGAudioContentSecure' => array(AttributeType::String, 'default' => null),
+					'socialOGAudioType'          => array(AttributeType::String, 'default' => null),
+					// OG Music - Song
+					'socialOGMusicSongDuration'  => array(AttributeType::String, 'default' => null),
+					'socialOGMusicSongAlbum'     => array(AttributeType::String, 'default' => null),
+					'socialOGMusicSongDisc'      => array(AttributeType::String, 'default' => null),
+					'socialOGMusicSongTrack'     => array(AttributeType::String, 'default' => null),
+					'socialOGMusicSongMusician' => array(AttributeType::String, 'default' => null),
+					// OG Profile
+					'socialOGProfileFirstName'   => array(AttributeType::String, 'default' => null),
+					'socialOGProfileLastName'    => array(AttributeType::String, 'default' => null),
+					'socialOGProfileUsername'    => array(AttributeType::String, 'default' => null),
+					'socialOGProfileGender'      => array(AttributeType::String, 'default' => null),
+					
+				// Twitter
+				'socialTwitterTitle'       => array(AttributeType::String, 'default' => null),
+				'socialTwitterType'        => array(AttributeType::Enum, 'values' => $twitterCardTypes, 'default' => $twitterCardTypes[0]),
+				'socialTwitterDescription' => array(AttributeType::String, 'default' => null),
+					// Twiter App
 					'socialTwitterAppCountry'          => array(AttributeType::String, 'default' => null),
 					'socialTwitterAppIphoneName'       => array(AttributeType::String, 'default' => null),
 					'socialTwitterAppIphoneId'         => array(AttributeType::String, 'default' => null),
@@ -71,15 +88,22 @@ class SimpleMeta_SimpleMetaModel extends BaseModel
 					'socialTwitterAppAndroidName'      => array(AttributeType::String, 'default' => null),
 					'socialTwitterAppAndroidId'        => array(AttributeType::String, 'default' => null),
 					'socialTwitterAppAndroidUrl'       => array(AttributeType::String, 'default' => null),
+					// Twiter Gallery
 					'socialTwitterGalleryImagesId'     => array(AttributeType::Number, 'default' => null), /* Asset */
+					// Twiter Photo
 					'socialTwitterPhotoId'             => array(AttributeType::Number, 'default' => null), /* Asset */
+					// Twiter Product
 					'socialTwitterProductData1'        => array(AttributeType::String, 'default' => null),
 					'socialTwitterProductLabel1'       => array(AttributeType::String, 'default' => null),
 					'socialTwitterProductData2'        => array(AttributeType::String, 'default' => null),
 					'socialTwitterProductLabel2'       => array(AttributeType::String, 'default' => null),
 					'socialTwitterProductImageId'      => array(AttributeType::Number, 'default' => null), /* Asset */
+					// Twiter Summary
 					'socialTwitterSummaryImageId'      => array(AttributeType::Number, 'default' => null), /* Asset */
+					// Twiter Summary w Large Image
 					'socialTwitterSummaryLargeImageId' => array(AttributeType::Number, 'default' => null), /* Asset */
+				
+				// SEO
 				'seoTitle'                           => array(AttributeType::String, 'default' => null),
 				'seoDescription'                     => array(AttributeType::String, 'default' => null),
 				'seoCanonicalUrl'                    => array(AttributeType::String, 'default' => null),
--- simplemeta/records/SimpleMeta_SimpleMetaRecord.php
+++ simplemeta/records/SimpleMeta_SimpleMetaRecord.php
@@ -16,9 +16,15 @@ class SimpleMeta_SimpleMetaRecord extends BaseRecord
 		// Open Graph Types
 		$openGraphTypes = array(
 			'article',
-			'audio',
+			'music.song',
+			'music.album',
+			'music.playlist',
+			'music.radio_station',
 			'profile',
 			'video.movie',
+			'video.episode',
+			'video.tv_show',
+			'video.other',
 			'website',
 		);
 
@@ -45,26 +51,37 @@ class SimpleMeta_SimpleMetaRecord extends BaseRecord
 		);
 
 		return array(
-				'handle'                         => AttributeType::String,
-				'socialOGTitle'                  => AttributeType::String,
-				'socialOGDescription'            => AttributeType::String,
-				'socialOGType'                   => array(AttributeType::Enum, 'values' => $openGraphTypes, 'default' => $openGraphTypes[0]),
-					// Open Graph Type fields
-					'socialOGAudioContentSecure'     => AttributeType::String,
-					'socialOGAudioType'              => AttributeType::String,
-					'socialOGProfileFirstName'       => AttributeType::String,
-					'socialOGProfileLastName'        => AttributeType::String,
-					'socialOGProfileUsername'        => AttributeType::String,
-					'socialOGProfileGender'          => AttributeType::String,
-					'socialOGVideoContentSecure'     => AttributeType::String,
-					'socialOGVideoUrl'				 => AttributeType::String,
-					'socialOGVideoType'              => AttributeType::String,
-					'socialOGVideoWidth'             => AttributeType::String,
-					'socialOGVideoHeight'            => AttributeType::String,
-				'socialTwitterTitle'             => AttributeType::String,
-				'socialTwitterType'              => array(AttributeType::Enum, 'values' => $twitterCardTypes, 'default' => $twitterCardTypes[0]),
-				'socialTwitterDescription'       => AttributeType::String,
-					// Twiter Card Type fields
+				'handle' => AttributeType::String,
+				// Open Graph
+					'socialOGTitle'              => AttributeType::String,
+					'socialOGDescription'        => AttributeType::String,
+					'socialOGType'               => array(AttributeType::Enum, 'values' => $openGraphTypes, 'default' => $openGraphTypes[0]),
+					// OG Video
+					'socialOGVideoContentSecure' => AttributeType::String,
+					'socialOGVideoUrl'           => AttributeType::String,
+					'socialOGVideoType'          => AttributeType::String,
+					'socialOGVideoWidth'         => AttributeType::String,
+					'socialOGVideoHeight'        => AttributeType::String,
+					// OG Audio
+					'socialOGAudioContentSecure' => AttributeType::String,
+					'socialOGAudioType'          => AttributeType::String,
+					// OG Music - Song
+					'socialOGMusicSongDuration'  => AttributeType::String,
+					'socialOGMusicSongAlbum'     => AttributeType::String,
+					'socialOGMusicSongDisc'      => AttributeType::String,
+					'socialOGMusicSongTrack'     => AttributeType::String,
+					'socialOGMusicSongMusician'  => AttributeType::String,
+					// OG Profile
+					'socialOGProfileFirstName'   => AttributeType::String,
+					'socialOGProfileLastName'    => AttributeType::String,
+					'socialOGProfileUsername'    => AttributeType::String,
+					'socialOGProfileGender'      => AttributeType::String,
+				
+				// Twitter	
+				'socialTwitterTitle'       => AttributeType::String,
+				'socialTwitterType'        => array(AttributeType::Enum, 'values' => $twitterCardTypes, 'default' => $twitterCardTypes[0]),
+				'socialTwitterDescription' => AttributeType::String,
+					// Twiter App
 					'socialTwitterAppCountry'     => AttributeType::String,
 					'socialTwitterAppIphoneName'  => array(AttributeType::String, 'default' => null),
 					'socialTwitterAppIphoneId'    => AttributeType::String,
@@ -75,16 +92,19 @@ class SimpleMeta_SimpleMetaRecord extends BaseRecord
 					'socialTwitterAppAndroidName' => array(AttributeType::String, 'default' => null),
 					'socialTwitterAppAndroidId'   => AttributeType::String,
 					'socialTwitterAppAndroidUrl'  => AttributeType::String,
+					// Twitter Product
 					'socialTwitterProductData1'   => AttributeType::String,
 					'socialTwitterProductLabel1'  => AttributeType::String,
 					'socialTwitterProductData2'   => AttributeType::String,
 					'socialTwitterProductLabel2'  => AttributeType::String,
-				'seoTitle'                       => AttributeType::String,
-				'seoDescription'                 => AttributeType::String,
-				'seoCanonicalUrl'                => AttributeType::String,
-				'seoRobotsIndex'                 => array(AttributeType::Enum, 'values' => $robotsIndex, 'default' => $robotsIndex[0]),
-				'seoRobotsFollow'                => array(AttributeType::Enum, 'values' => $robotsFollow, 'default' => $robotsFollow[0]),
-				'seoSitemapPriority'             => array(AttributeType::String, 'default' => '0.5'),
+				
+				// SEO
+				'seoTitle'           => AttributeType::String,
+				'seoDescription'     => AttributeType::String,
+				'seoCanonicalUrl'    => AttributeType::String,
+				'seoRobotsIndex'     => array(AttributeType::Enum, 'values' => $robotsIndex, 'default' => $robotsIndex[0]),
+				'seoRobotsFollow'    => array(AttributeType::Enum, 'values' => $robotsFollow, 'default' => $robotsFollow[0]),
+				'seoSitemapPriority' => array(AttributeType::String, 'default' => '0.5'),
 		);
 
 	}
